bdc0e44ed13d6b523ffc5d9eaffa13f93bb2019a
Mr Benjamin Mather <102623743+CrumpetCrusader@users.noreply.github.com>
2025-02-02 18:03:05 +0000

    Update prCol.ps1

--- prCol.ps1
+++ prCol.ps1
@@ -1,34 +1,35 @@
 $RegistryPath   = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent'
 
-#RED VALUES
-$RAccpal        = '5f,ff,a5,00,26,ff,8e,00,00,e7,75,00,00,cc,6a,00,00,b2,5a,00,00,76,35,00,00,3f,13,00,e3,00,8c,00'
-$RValueS        = 'ff5ab200'
-$RValueA        = 'ff6acc00'
+# RED VALUES
+$RAccPal        = [byte[]](0x5f,0xff,0xa5,0x00,0x26,0xff,0x8e,0x00,0x00,0xe7,0x75,0x00,0x00,0xcc,0x6a,0x00,0x00,0xb2,0x5a,0x00,0x00,0x76,0x35,0x00,0x00,0x3f,0x13,0x00,0xe3,0x00,0x8c,0x00)
+$RValueS        = 0xff5ab200
+$RValueA        = 0xff6acc00
 
-#GREEN VALUES
-$GAccPal        = 'f7,b1,a5,00,e8,80,7a,00,d8,4b,4c,00,d1,34,38,00,be,2b,2e,00,8d,1a,1c,00,61,09,0a,00,64,7c,64,00'
-$GValueS        = 'ff2e2bbe'
-$GValueA        = 'ff3834d1'
+# GREEN VALUES
+$GAccPal        = [byte[]](0xf7,0xb1,0xa5,0x00,0xe8,0x80,0x7a,0x00,0xd8,0x4b,0x4c,0x00,0xd1,0x34,0x38,0x00,0xbe,0x2b,0x2e,0x00,0x8d,0x1a,0x1c,0x00,0x61,0x09,0x0a,0x00,0x64,0x7c,0x64,0x00)
+$GValueS        = 0xff2e2bbe
+$GValueA        = 0xff3834d1
 
 # Determine power source
 function Get-PowerSource {
-    $powerStatus = (Get-WmiObject -Namespace root\WMI -Class BatteryStatus).PowerOnline
+    $powerStatus = (Get-WmiObject -Query "SELECT * FROM Win32_Battery").BatteryStatus
     return $powerStatus
 }
 
 # Main script
 $powerSource = Get-PowerSource
 
-if ($powerSource -eq $true) {
+if ($powerSource -eq 2) {
     # AC power (plugged in) - green accent color
-    Set-ItemProperty -Path $RegistryPath -Name AccentPalette -Value $RAccPal -Type HEX -Force
-    Set-ItemProperty -Path $RegistryPath -Name StartColorMenu -Value $RValueS -Type DWORD -Force 
-    Set-ItemProperty -Path $RegistryPath -Name AccentColorMenu -Value $RValueA -Type DWORD -Force  
-    Stop-Process -Name explorer -Force
+    Set-ItemProperty -Path $RegistryPath -Name AccentPalette -Value $GAccPal -Force
+    Set-ItemProperty -Path $RegistryPath -Name StartColorMenu -Value $GValueS -Force 
+    Set-ItemProperty -Path $RegistryPath -Name AccentColorMenu -Value $GValueA -Force  
 } else {
     # DC power (battery) - red accent color
-    Set-ItemProperty -Path $RegistryPath -Name AccentPalette -Value $GAccPal -Type HEX -Force
-    Set-ItemProperty -Path $RegistryPath -Name StartColorMenu -Value $GValueS -Type DWORD -Force 
-    Set-ItemProperty -Path $RegistryPath -Name AccentColorMenu -Value $GValueA -Type DWORD -Force 
-    Stop-Process -Name explorer -Force
+    Set-ItemProperty -Path $RegistryPath -Name AccentPalette -Value $RAccPal -Force
+    Set-ItemProperty -Path $RegistryPath -Name StartColorMenu -Value $RValueS -Force 
+    Set-ItemProperty -Path $RegistryPath -Name AccentColorMenu -Value $RValueA -Force 
 }
+
+# Restart explorer to apply changes
+Stop-Process -Name explorer -Force
